b269f4fe3619f15717676d86f37bdb506681aa4a
Adrian Chan <adrian.kaho.chan@gmail.com>
2025-05-19 16:13:04 +0200

    with / without replicates

--- volcano_plot_mod_level_changes.py
+++ volcano_plot_mod_level_changes.py
@@ -11,8 +11,12 @@ from tqdm import tqdm
 def get_vec_change_neg_log_pval(in_dmr_file):
     change_pval = []
     for this_chunk in tqdm(pd.read_csv(in_dmr_file, sep='\t', dtype={'chrom': str}, iterator=True, chunksize=10000)):
-        this_chunk_valid = this_chunk[this_chunk['p_value'] < 1]
-        change_pval.append(this_chunk_valid[['effect_size', 'p_value']].values)
+        if replicates:
+            this_chunk_valid = this_chunk[this_chunk['balanced_p_value'] < 1]
+            change_pval.append(this_chunk_valid[['balanced_effect_size', 'balanced_p_value']].values)
+        else:
+            this_chunk_valid = this_chunk[this_chunk['p_value'] < 1]
+            change_pval.append(this_chunk_valid[['effect_size', 'p_value']].values)
     vec_effect_size, vec_pval = np.vstack(change_pval).T
     vec_change = -vec_effect_size * 100
     vec_neg_log_pval = -np.log10(vec_pval)
@@ -42,14 +46,18 @@ dict_display_mod = {
 
 # exp = 'HEK293_psU-KD'
 exp = 'HEK293_psU-OE'
-cond0 = 'CTRL-KD'
-cond1 = 'PUS7-OE'
+cond0 = 'CTRL_rep1'
+cond1 = 'CTRL_rep2'
+replicates = False
 
 thresh_neg_log_pval = 2.0
 
 plt.figure(figsize=(10, 5))
 for subplot_ind, mod in enumerate(['psi', 'm6A']):
-    this_dmr_file = os.path.join(data_dir, exp, f'{cond0}_{cond1}.cov10.{mod}.diff_sites.dmr')
+    if replicates:
+        this_dmr_file = os.path.join(data_dir, exp, 'dmr', f'{cond0}_{cond1}_replicates.cov10.{mod}.diff_sites.dmr')
+    else:
+        this_dmr_file = os.path.join(data_dir, exp, 'dmr', f'{cond0}_{cond1}.cov10.{mod}.diff_sites.dmr')
     this_vec_change, this_vec_neg_log_pval = get_vec_change_neg_log_pval(this_dmr_file)
     this_mask_no_change, this_mask_neg_change, this_mask_pos_change, this_num_neg_change, this_num_pos_change = get_mask(this_vec_change, this_vec_neg_log_pval, thresh_neg_log_pval)
 
@@ -66,5 +74,9 @@ for subplot_ind, mod in enumerate(['psi', 'm6A']):
     # plt.ylim([0, 20])
     plt.text(0.01, 1.01, f'{this_num_neg_change} down', c='blue', ha='left', transform=plt.gca().transAxes)
     plt.text(0.99, 1.01, f'{this_num_pos_change} up', c='red', ha='right', transform=plt.gca().transAxes)
-plt.suptitle(f'{cond1} versus {cond0}')
-plt.savefig(os.path.join(img_out, f'volcano_{exp}_{cond0}_{cond1}_diff_sites.png'), bbox_inches='tight')
+if replicates:
+    plt.suptitle(f'{cond1} versus {cond0}, with replicates')
+    plt.savefig(os.path.join(img_out, f'volcano_{exp}_{cond0}_{cond1}_replicates_diff_sites.png'), bbox_inches='tight')
+else:
+    plt.suptitle(f'{cond1} versus {cond0}')
+    plt.savefig(os.path.join(img_out, f'volcano_{exp}_{cond0}_{cond1}_diff_sites.png'), bbox_inches='tight')
