2be9f3e93048dc422998ed3eee1dead08a5643d6
Adrian <adrian.kaho.chan@gmail.com>
2025-05-26 11:46:45 +0200

    modkit dmr -- assign canonical base to new mod code

--- run_modkit_dmr.sh
+++ run_modkit_dmr.sh
@@ -15,7 +15,7 @@ out_dir="/prj/TRR319_RMaP_BaseCalling_RNA004/Adrian/HEK293_M3I/dmr"
 ref="/biodb/genomes/homo_sapiens/GRCh38_102/GRCh38_102.fa"
 
 cond0=DMSO_rep1
-cond1=M3I_24h_rep1
+cond1=M3I_6h_rep1
 
 #mod=m6A
 #base=A
@@ -27,6 +27,10 @@ modkit dmr pair \
   -b ${in_dir}/${cond1}.cov10.bedmethyl.gz \
   -o ${out_dir}/${cond0}_${cond1}.cov10.${mod}.dmr \
   --ref ${ref} \
+  --assign-code 19227:T \
+  --assign-code 19228:C \
+  --assign-code 19229:G \
+  --assign-code 69426:A \
   --base ${base} \
   --threads 36 \
   --log-filepath ${out_dir}/${cond0}_${cond1}.cov10.${mod}.log
